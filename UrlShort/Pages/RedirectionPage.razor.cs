efb59d8d8bfd94de53168d52e44d4967288229b9
mikejke <mikejke99@gmail.com>
2020-12-04 21:43:11 +0300

    removed table-responsive class

---
+++ UrlShort/Pages/RedirectionPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace UrlShort.Pages
+{
+    public partial class RedirectionPage
+    {
+        [Parameter] 
+        public string ShortUrl { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await using var context = DbFactory.CreateDbContext();
+            var url = await context.ShortUrls.FirstOrDefaultAsync(u => u.Short == ShortUrl);
+
+            if (url != null)
+            {
+                url.Clicks++;
+                context.Entry(url).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                NavigationManager.NavigateTo(new[] {"http", "://"}.Any(s => url.Full.Contains(s)) ? url.Full : $"http://{url.Full}");
+            }
+            else
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
